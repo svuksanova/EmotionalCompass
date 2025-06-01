40f07e86c0c24f35f1634c86edf755fb52cdfbd0
Stefani <46635188+svuksanova@users.noreply.github.com>
2025-06-01 21:04:35 +0200

    Add Routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './css/index.css'
-import Login from './components/Login.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import './css/index.css';
+
+import LoginPage   from './pages/LoginPage';
 
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Login />
-  </StrictMode>,
-)
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                {/* public */}
+                <Route path="/login"  element={<LoginPage />} />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
+);
